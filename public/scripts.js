e0e2d67e7d79a6992afbae87327897f0905c3501
Mariam Rafique <mrafique@unc.edu>
2023-05-09 10:33:10 -0400

    add css

--- public/scripts.js
+++ public/scripts.js
@@ -2,9 +2,8 @@
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
 
-const messageStatus = document.getElementById("status_message")
+const statusMessage = document.getElementById("status_message")
 
-//player choice
 const capitalize = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
@@ -12,28 +11,22 @@ const capitalize = (string) => {
 const showOptions = (event) => {
   document.getElementById("play_form").removeAttribute("hidden");
 
-
-  //rps game 
   if(event.target.id === "rps") {
     document.getElementById("extra_options").setAttribute("hidden", "true");
-    //rpsls game 
   } else if (event.target.id === "rpsls") {
     document.getElementById("extra_options").removeAttribute("hidden");
   }
 
   if(event.target.id === "random") {
-  	//random choice by player
     document.getElementById("opponent_options").setAttribute("hidden", "true");
     document.getElementById("random_options").removeAttribute("hidden");
-  } 
-  else if(event.target.id === "opponent") {
+  } else if(event.target.id === "opponent") {
     document.getElementById("random_options").setAttribute("hidden", "true");
     document.getElementById("opponent_options").removeAttribute("hidden");
   }
 }
-//rest button options for player
+
 const resetOptions = () => {
-  
   document.getElementById("play_form").setAttribute("hidden", "true");
   document.getElementById("opponent_options").setAttribute("hidden", "true");
   document.getElementById("random_options").setAttribute("hidden", "true");
@@ -41,30 +34,26 @@ const resetOptions = () => {
 }
 
 const submit = (event) => {
-  
-  const typeofGame = document.querySelector('input[name="typeofGame"]:checked')?.value;
-  
-  const playGame = document.querySelector('input[name="playGame"]:checked')?.value;
+  const gameType = document.querySelector('input[name="game_type"]:checked')?.value;
+  const playstyle = document.querySelector('input[name="playstyle"]:checked')?.value;
 
-  //main player 
-  if(typeofGame && playGame) {
+  if(gameType && playstyle) {
     const getData = (player) => {
-      fetch(`/app/${typeofGame}/play/${player}`)
+      fetch(`/app/${gameType}/play/${player}`)
       .then((res) => {
         return res.json();
       })
       .then((json) => {
-        messageStatus.innerText = `You played: ${capitalize(json.player)}\nThe opponent played: ${capitalize(json.opponent)}\nResult: ${capitalize(json.result)}`;
+        statusMessage.innerText = `You played: ${capitalize(json.player)}\nThe opponent played: ${capitalize(json.opponent)}\nResult: ${capitalize(json.result)}`;
       })
       .catch((e) => {
-        messageStatus.innerText = 'Something went wrong. Please try again!'
+        statusMessage.innerText = 'Something went wrong. Please try again!'
       });
     }
-    //opponent player 
-    if(playGame === "opponent") {
+    if(playstyle === "opponent") {
       getData(document.querySelector('input[name="player_option"]:checked')?.value);
     } else {
-      fetch(`/app/${typeofGame}`)
+      fetch(`/app/${gameType}`)
       .then((res) => {
         return res.json();
       })
@@ -74,33 +63,19 @@ const submit = (event) => {
     }
   }
 }
-//buttons
-//each time button is pressed, sets class to active 
-//documentations
 
 const rulesModal = document.getElementById("how_to_play");
-
 const openRulesButton = document.getElementById("show_rules");
-
 const closeRulesButton = document.getElementById("close_rules");
-
 const gameSelect = document.getElementById("form");
 const randomSubmitButton = document.getElementById("random_submit");
-
 const opponentSubmitButton = document.getElementById("opponent_submit");
 
-
-//buttons 
-
 gameSelect.addEventListener("input", showOptions);
-
 gameSelect.addEventListener("reset", resetOptions);
 
 randomSubmitButton.addEventListener("click", submit);
-
 opponentSubmitButton.addEventListener("click", submit);
 
 openRulesButton.addEventListener("click", () => rulesModal.style.display = "block");
-
-
 closeRulesButton.addEventListener("click", () => rulesModal.style.display = "none")
